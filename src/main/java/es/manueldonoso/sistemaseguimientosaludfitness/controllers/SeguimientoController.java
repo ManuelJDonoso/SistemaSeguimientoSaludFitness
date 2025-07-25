91f437daf3e0c2bb85d51b2064beee50eec4beef
Manuel Jesús Donoso Pérez <dev@manueldonoso.es>
2025-07-25 21:36:34 +0200

    feat(seguimiento): añadir campos de cliente y tabla a la interfaz y controlador
    - Se añadieron nuevos controles a SeguimientoController y Seguimiento.fxml:
      campos para DNI, nombre, apellidos, población y fecha de cita.
    - Se integró una TableView para mostrar los datos del cliente en la vista de seguimiento.
    
    Este commit prepara la base visual y lógica para el seguimiento de clientes,
    facilitando futuras funcionalidades como edición, filtrado y registro de citas.

--- src/main/java/es/manueldonoso/sistemaseguimientosaludfitness/controllers/SeguimientoController.java
+++ src/main/java/es/manueldonoso/sistemaseguimientosaludfitness/controllers/SeguimientoController.java
@@ -4,9 +4,15 @@
  */
 package es.manueldonoso.sistemaseguimientosaludfitness.controllers;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -14,6 +20,27 @@ import javafx.fxml.Initializable;
  * @author donpe
  */
 public class SeguimientoController implements Initializable {
+
+    @FXML
+    private JFXTextField tf_dni;
+    @FXML
+    private JFXTextField tf_nombre;
+    @FXML
+    private JFXComboBox<?> cb_poblacion;
+    @FXML
+    private JFXTextField tf_apellidos;
+    @FXML
+    private DatePicker fp_fecha_cita;
+    @FXML
+    private TableView<?> tabla_clientes;
+    @FXML
+    private TableColumn<?, ?> cl_dni;
+    @FXML
+    private TableColumn<?, ?> cl_clientes;
+    @FXML
+    private TableColumn<?, ?> cl_poblacion;
+    @FXML
+    private TableColumn<?, ?> cl_cita;
 
     /**
      * Initializes the controller class.
