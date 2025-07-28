b1bde9cbb7ed881dbb6ea0dedeff49d372905ef4
Manuel Jesús Donoso Pérez <dev@manueldonoso.es>
2025-07-29 01:55:44 +0200

    feat(fichaCliente): ampliar la interfaz con nuevos campos y botones
    Se actualizó FichaClientes.fxml y FichaClientesController para incluir:
    - Nuevos campos de texto, área de observaciones y DateTimePicker.
    - Controladores de eventos de botón.
    - Mejoras en la organización visual y funcional.

--- src/main/java/es/manueldonoso/sistemaseguimientosaludfitness/controllers/FichaClientesController.java
+++ src/main/java/es/manueldonoso/sistemaseguimientosaludfitness/controllers/FichaClientesController.java
@@ -4,9 +4,13 @@
  */
 package es.manueldonoso.sistemaseguimientosaludfitness.controllers;
 
+import com.browniebytes.javafx.control.DateTimePicker;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import es.manueldonoso.sistemaseguimientosaludfitness.models.Cliente;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -24,6 +28,30 @@ public class FichaClientesController implements Initializable {
     private Cliente cliente;
     
     private String dni;
+    @FXML
+    private JFXTextField tf_cliente;
+    @FXML
+    private JFXTextField tf_dni;
+    @FXML
+    private JFXTextField tf_altura;
+    @FXML
+    private JFXTextField tf_ultimoPeso;
+    @FXML
+    private JFXTextField tf_pesoActual;
+    @FXML
+    private JFXTextField tf_grasaCorporal;
+    @FXML
+    private JFXTextField tf_proteina;
+    @FXML
+    private JFXTextField tf_metabolismoBasal;
+    @FXML
+    private JFXTextField tfGrasaVisceral;
+    @FXML
+    private JFXTextArea ta_Anotaciones;
+    @FXML
+    private DateTimePicker tf_fechaHoraTomasDatos;
+    @FXML
+    private JFXTextField tf_pesoIdeal;
 
 
     
@@ -44,6 +72,50 @@ public class FichaClientesController implements Initializable {
     public void cargarFicha(String dni){
         setDni(dni);
         System.out.println(dni);
+    }
+
+    @FXML
+    private void OA_btnDatosContactos(ActionEvent event) {
+    }
+
+    @FXML
+    private void oa_btnActualizarDatos(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_AsignarCita(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_Cancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_camara(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_subirImagen(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_historialDieta(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_establecerDieta(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_historialEjercicios(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_ejercicioSemanal(ActionEvent event) {
+    }
+
+    @FXML
+    private void OA_pdf(ActionEvent event) {
     }
     
 }
